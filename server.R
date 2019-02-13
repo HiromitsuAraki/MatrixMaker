990a09b133fc241f2b8b37de5a1047243b0b9e2e
HiromitsuAraki <46471150+HiromitsuAraki@users.noreply.github.com>
2019-02-13 15:55:58 +0900

    Update server.R

--- server.R
+++ server.R
@@ -33,7 +33,12 @@ shinyServer(function(input, output, session) {
                    if (as.character(input$checkGenome)=="hg38"){
                      if (input$checkFeature==1)  gf=fread("./annotation/hg38_CGI")
                      if (input$checkFeature==2)  gf=fread("./annotation/refGene_hg38_gb_sorted")
-                     if (input$checkFeature>=3)  gf=fread("./annotation/refGene_hg38_1stIntron")
+                     
+                     #if (input$checkFeature>=3)  gf=fread("./annotation/refGene_hg38_1stIntron")
+                     if (input$checkFeature==3)  gf=fread("./annotation/refGene_hg38_1stIntron")
+                     if (input$checkFeature==4)  gf=fread("./annotation/refGene_hg38_gb_sorted")
+                     
+                     
                      if (input$checkPlatform==2) Infinium=readRDS("./annotation/EPIC.hg38.manifest.addressA.rds")
                      if (input$checkPlatform==3) Infinium=readRDS("./annotation/hm450.hg38.manifest.addressA.rds")
                    }
@@ -41,7 +46,11 @@ shinyServer(function(input, output, session) {
                    if (as.character(input$checkGenome)=="hg19"){
                      if (input$checkFeature==1)  gf=fread("./annotation/hg19_CGI")
                      if (input$checkFeature==2)  gf=fread("./annotation/refGene_hg19_gb_sorted")
-                     if (input$checkFeature>=3)  gf=fread("./annotation/refGene_hg19_1stIntron")
+                     
+                     #if (input$checkFeature>=3)  gf=fread("./annotation/refGene_hg19_1stIntron")
+                     if (input$checkFeature==3)  gf=fread("./annotation/refGene_hg19_1stIntron")
+                     if (input$checkFeature==4)  gf=fread("./annotation/refGene_hg19_gb_sorted")
+             
                      if (input$checkPlatform==2) Infinium=readRDS("./annotation/EPIC.hg19.manifest.addressA.rds")
                      if (input$checkPlatform==3) Infinium=readRDS("./annotation/hm450.hg19.manifest.addressA.rds")
                    }
@@ -49,13 +58,17 @@ shinyServer(function(input, output, session) {
                    if (as.character(input$checkGenome)=="mm10"){
                      if (input$checkFeature==1) gf=fread("./annotation/mm10_CGI")
                      if (input$checkFeature==2) gf=fread("./annotation/refGene_mm10_gb_sorted")
-                     if (input$checkFeature>=3) gf=fread("./annotation/refGene_mm10_1stIntron")
+                    # if (input$checkFeature>=3) gf=fread("./annotation/refGene_mm10_1stIntron")
+                      if (input$checkFeature==3) gf=fread("./annotation/refGene_mm10_1stIntron")
+                      if (input$checkFeature==4) gf=fread("./annotation/refGene_mm10_gb_sorted")
                    }
                   
                    if (as.character(input$checkGenome)=="mm9"){
                      if (input$checkFeature==1) gf=fread("./annotation/mm9_CGI")
                      if (input$checkFeature==2) gf=fread("./annotation/refGene_mm9_gb_sorted")
-                     if (input$checkFeature>=3) gf=fread("./annotation/refGene_mm9_1stIntron")
+                     #if (input$checkFeature>=3) gf=fread("./annotation/refGene_mm9_1stIntron")
+                     if (input$checkFeature==3) gf=fread("./annotation/refGene_mm9_1stIntron")
+                     if (input$checkFeature==4) gf=fread("./annotation/refGene_mm9_gb_sorted")
                    }
 
                    
